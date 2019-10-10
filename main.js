210860d8e901f6e144ce0c3eaf432e1e43d6d0a5
grisumbras <grisumbras@users.noreply.github.com>
2019-10-10 13:51:56 +0000

    Automated deployment: Thu Oct 10 13:51:56 UTC 2019 0c0f94df9017b85d3f84fc5d19dcd99cf554dd2e

---
+++ main.js
@@ -0,0 +1,106 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+
+function get_env(name) { return (process.env[name] || '').trim(); }
+
+async function get_output(command, ...args) {
+  let output = '';
+  const opts = {
+    listeners: { stdout: (data) => { output += data.toString(); } }
+  };
+  await exec.exec(command, args, opts);
+
+  output = output.trim();
+  return 'None' != output ? output : '' ;
+}
+
+async function inspect_pkg(attr) {
+  return await get_output('conan', 'inspect', '.', '--raw', attr);
+}
+
+async function get_input_or_pkg_attr(attr) {
+  let result = core.getInput(attr);
+  if (!result) { result  = await inspect_pkg(attr); }
+  return result;
+}
+
+async function get_pkg_user() {
+  let result = core.getInput('user');
+
+  if (!result) { result = get_env('CONAN_USERNAME'); }
+
+  if (!result) { result = await inspect_pkg('default_user'); }
+
+  if (!result) {
+    const repo = get_env('GITHUB_REPOSITORY');
+    result = (repo.split('/', 1) || [ '' ])[0].trim();
+  }
+
+  return result;
+}
+
+async function get_pkg_channel() {
+  let result = core.getInput('channel');
+
+  if (!result) { result = get_env('CONAN_CHANNEL'); }
+
+  if (!result) { result = await inspect_pkg('default_channel'); }
+
+  if (!result) { result = 'testing'; }
+
+  return result;
+}
+
+async function get_pkg_reference() {
+  let result = core.getInput('reference');
+  if (!result) {
+    const name = await get_input_or_pkg_attr('name');
+    const version = await get_input_or_pkg_attr('version');
+    const user = await get_pkg_user();
+    const channel = await get_pkg_channel();
+    result = `${name}/${version}@${user}/${channel}`
+  }
+  return result;
+}
+
+async function get_upload_remote() {
+  let result = {}
+
+  result.name = core.getInput('remote');
+  if (!result.name) { result.name = 'upload'; }
+
+  result.url = core.getInput('url');
+  if (!result.url) { result.url = get_env('CONAN_UPLOAD'); }
+
+  return result;
+}
+
+async function get_login_username() {
+  let result = core.getInput('login');
+  if (!result) { result = await get_pkg_user(); }
+  return result;
+}
+
+async function run() {
+  const remote = await get_upload_remote();
+  console.log()
+  console.log(`Using remote ${remote.name}(${remote.url})`);
+  await exec.exec('conan', ['remote', 'add', remote.name, remote.url]);
+
+  const user = await get_login_username()
+  const password = core.getInput('password');
+  console.log()
+  console.log(`Authenticating as ${user}`);
+  await exec.exec('conan', ['user', user, '-r', remote.name, '-p', password]);
+
+  const pkg_reference = await get_pkg_reference();
+  console.log()
+  console.log(`Using full package reference ${pkg_reference}`);
+
+  await exec.exec(
+    'conan', ['upload', pkg_reference, '-r', remote.name, '--all', '--confirm']
+  );
+}
+
+run()
